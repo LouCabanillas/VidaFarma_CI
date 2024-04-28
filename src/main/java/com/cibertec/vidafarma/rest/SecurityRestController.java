b61b7089508dc79adcb2bb4153bff1aafb326b14
Lourdes Cabanillas <lulcg07@gmail.com>
2024-04-27 19:03:29 -0500

    cambios

---
+++ src/main/java/com/cibertec/vidafarma/rest/SecurityRestController.java
@@ -0,0 +1,27 @@
+package com.cibertec.vidafarma.rest;
+
+import com.cibertec.vidafarma.entity.ClienteEntity;
+import com.cibertec.vidafarma.entity.ProductoEntity;
+import com.cibertec.vidafarma.enums.Role;
+import com.cibertec.vidafarma.services.ClienteService;
+import com.cibertec.vidafarma.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/security")
+public class SecurityRestController {
+
+    @Autowired
+    ClienteService service;
+
+
+    @PostMapping("/registrar")
+    public Long registrar(ClienteEntity cliente) {
+        cliente.setRole(Role.USER);
+        return service.crear(cliente);
+    }
+
+}
